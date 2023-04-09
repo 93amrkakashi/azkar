15bb7809dcc551ff099486da86b6215f0f764c24
93amr.kakashi <93amr.kakashi@gmail.com>
2023-04-09 11:58:44 +0700

    added type script

---
+++ app-test.ts
@@ -0,0 +1,15 @@
+const counter: NodeListOf<HTMLHeadingElement> =
+  document.querySelectorAll(".counter .num");
+
+counter.forEach((count) => {
+  let result: number = +count.innerHTML;
+  count.onclick = function () {
+    result -= 1;
+    count.innerHTML = `${result}`;
+    if (result <= 0) {
+      count.classList.add("done");
+      result = 0;
+      count.innerHTML = "0";
+    }
+  };
+});
--- app.js
+++ app.js
@@ -1,77 +1,22 @@
-const counter = document.querySelectorAll(".counter .num");
-let inbut = document.querySelectorAll(".real");
-const btn = document.querySelectorAll(".btn");
-let dec = 0;
-
-// console.log(parseInt(inbut) +2)
-
-// console.log(btn)
-
-// console.log(btn)
+// const counter = document.querySelectorAll(".counter .num");
+// let inbut = document.querySelectorAll(".real");
+// const btn = document.querySelectorAll(".btn");
+// let dec = 0;
 
 
-// inbut.innerHTML =('566')
-    counter.forEach((count) => {
-        console.log(this);
-        count.onclick = function () {
+//     counter.forEach((count) => {
+//         console.log(this);
+//         count.onclick = function () {
             
-            count.innerHTML = parseInt(count.innerHTML - 1)
-            if (count.innerHTML <= 0) {
-                count.classList.add("done");
-                count.innerHTML = 0     
-            }
+//             count.innerHTML = parseInt(count.innerHTML - 1)
+//             if (count.innerHTML <= 0) {
+//                 count.classList.add("done");
+//                 count.innerHTML = 0     
+//             }
 
             
-        }
+//         }
 
-        // dec = dec+1;
-        // this.innerHTML = `${dec}`
-        // if (ele.innerHTML !== counter.innerHTML) {
-        //     console.log("yes");
-        //     dec+=1
-        //     inbut.innerHTML = `${dec}`
-
-        //     inbut.innerHTML = counter.innerHTML
-        //     // inbut.value = counter
-        // }
-        //  else {
-        //     console.log(inbut.innerHTMl)
-        //     // console.log(dec)
-        //     dec+=1
-        //     inbut.innerHTML = `${dec}`
-        // }
-    });
-
-
-
-// btn.forEach((ele) => {
-//     ele.onclick = change
-//     // console.log(this);
-//     // console.log(inbut);
-
-
-//     // parseInt(counter) + 1
-//     // parseInt(counter)
-//     // counter.forEach((count) => {
-//     // console.log(count);
-
-//     //     // dec = dec+1;
-//     //     count.innerHTML =  `${dec}`
-//     //     // if (ele.innerHTML !== counter.innerHTML) {
-//     //     //     console.log("yes");
-//     //     //     dec+=1
-//     //     //     inbut.innerHTML = `${dec}`
-
-//     //     //     inbut.innerHTML = counter.innerHTML
-//     //     //     // inbut.value = counter
-//     //     // }
-//     //     //  else {
-//     //     //     console.log(inbut.innerHTMl)
-//     //     //     // console.log(dec)
-//     //     //     dec+=1
-//     //     inbut.innerHTML = `${dec}`
-//     // }
-// });
-
-
+        
+//     });
 
