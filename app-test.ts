25080b44491033949039cf5826ff478b3b3efd57
93amrkakashi <93amr.kakashi>
2023-08-16 04:04:10 +0200

    sdsa

--- app-test.ts
+++ app-test.ts
@@ -6,6 +6,7 @@ counter.forEach((count) => {
   count.onclick = function () {
     result -= 1;
     count.innerHTML = `${result}`;
+    
     if (result <= 0) {
       count.classList.add("done");
       result = 0;
